0ff9518ccbf24d609bb62d0f2c0dfe0822d7e497
Miguel Beltran <m.beltrann676@gmail.com>
2023-06-11 12:30:00 -0700

    Updated Selenium, Inserted WebDriver

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 # ChromeDriver is what connects Selenium to Chrome
 chromeDriverPath = "/Users/mig/Development/chromedriver_mac_arm64/chromedriver"
 # chrome_options = Options()
 # chrome_options.add_experimental_option("detach", True)
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver = webdriver.Chrome(executable_path=chromeDriverPath)
 
-driver.get("https://www.amazon.com")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
+
 # input_text = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[2]/a')
 # print(input_text.text)
 
