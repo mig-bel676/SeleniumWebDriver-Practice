cb5e9361c367aa8c31ea15e557d8ba11d93ca82e
Miguel Beltran <m.beltrann676@gmail.com>
2023-06-12 15:00:07 -0700

    Testing Selenium Driver, Used find elements function with By class

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
+
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 # ChromeDriver is what connects Selenium to Chrome
 chromeDriverPath = "/Users/mig/Development/chromedriver_mac_arm64/chromedriver"
-# chrome_options = Options()
-# chrome_options.add_experimental_option("detach", True)
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.google.com")
-
-# input_text = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[2]/a')
-# print(input_text.text)
+chrome_options = Options()
+# Keeps browser open
+chrome_options.add_experimental_option("detach", True)
 
 
-# driver.quit()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+driver.get("https://www.python.org")
+
+searchbar = driver.find_element(By.NAME, "q")
+
+print(searchbar.siz)
+
+
+
